ec0a4100fb0f198e7433b30549cd2b9f86c8a8bc
Walter <73754678+buh-kiddo@users.noreply.github.com>
2023-09-23 18:00:26 +0300

    Update callback.php

--- darajaapp/callback.php
+++ darajaapp/callback.php
@@ -1,8 +1,8 @@
 <?php
 
-header("content-Type: application/json");
+header("Content-Type: application/json");
 $stkCallbackResponse = file_get_contents('php://input');
-$logFile = "Mpesastkreponse.json";
+$logFile = "Mpesastkresponse.json";
 $log = fopen($logFile, "a");
 fwrite($log, $stkCallbackResponse);
 fclose($log);
